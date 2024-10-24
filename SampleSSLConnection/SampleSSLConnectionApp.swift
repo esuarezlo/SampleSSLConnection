82df90bcb494a4bb63fea5305ae35dde381c5ef6
Ernesto <esuarezlo@unsa.edu.pe>
2024-10-23 23:07:19 -0500

    Initial Commit

---
+++ SampleSSLConnection/SampleSSLConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSSLConnectionApp.swift
+//  SampleSSLConnection
+//
+//  Created by epismac on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleSSLConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
